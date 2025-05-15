a164716ff90a09f64a12efe5abf317a1f935235c
shikurassefa <jabirshikur@gmail.com>
2025-05-15 09:17:14 +0300

    importing React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Cart/index.css';
 import App from './Cart/App'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
